bea0a61a85f0efaab0f5f5c2c4de313012ea204e
techguruCD <colinkong0406@yahoo.com>
2023-07-27 12:52:42 -0700

    ?

--- js/customize.js
+++ js/customize.js
@@ -3,8 +3,7 @@ gsap.registerPlugin(ScrollTrigger);
 let sections = gsap.utils.toArray(".slider-item");
 let sliderEle = document.querySelector(".slider-item");
 let guideboxs = document.querySelector(".guide-box");
-
-console.log(guideboxs);
+let earthBackground = document.querySelector(".earth-background");
 
 ScrollTrigger.matchMedia({
   "(maxn-width: 768px)": function () {
@@ -46,6 +45,7 @@ ScrollTrigger.matchMedia({
       xPercent: -100 * (sections.length - 1),
       // ease: "none",
       scrollTrigger: {
+        // trigger: ".earth-section",
         trigger: ".chip-horizontal-slider",
         pin: true,
         scrub: 1,
@@ -55,22 +55,22 @@ ScrollTrigger.matchMedia({
           document.querySelector(".earth-slider-pos-container.top").style.height = guideLineHeight + "px";
           document.querySelector(".earth-slider-pos-container.down").style.height = guideLineHeight + "px";
           return "-" + (document.documentElement.clientHeight - document.querySelector(".chip-horizontal-slider").offsetHeight) / 2;
+          // return "top -" + (document.querySelector(".chip-introduction").offsetHeight + 221);
         },
         end: () => "+=" + document.querySelector(".chip-horizontal-slider").offsetWidth * (sections.length - 1)
+        // end: "+=10000"
       }
     });
 
-    guide_box = ".guide-box";
-    timeline1 = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.chip-horizontal-slider',
-        scrub: 1,
-        end: "+=" + document.querySelector(".chip-horizontal-slider").offsetWidth * (sections.length - 1),
-        pin: true
-      }
-    });
-    timeline1.to(guide_box, { y: () => (document.documentElement.clientHeight - document.querySelector(".chip-horizontal-slider .slider-container").offsetHeight) / 2 - 20 })
-
-    // gsap.to(".box", {y: })
+    // guide_box = ".guide-box";
+    // timeline1 = gsap.timeline({
+    //   scrollTrigger: {
+    //     trigger: '.chip-horizontal-slider',
+    //     scrub: 1,
+    //     end: "+=" + document.querySelector(".chip-horizontal-slider").offsetWidth * (sections.length - 1),
+    //     pin: true
+    //   }
+    // });
+    // timeline1.to(guide_box, { y: () => (document.documentElement.clientHeight - document.querySelector(".chip-horizontal-slider .slider-container").offsetHeight) / 2 - 15 })
   }
 });
